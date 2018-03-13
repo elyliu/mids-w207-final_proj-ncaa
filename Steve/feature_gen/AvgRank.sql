b3b5154ad0a5a97e9f5c76e02e4bf44ef9335386
syang <steven.s.yang@gmail.com>
2018-03-13 15:54:40 -0700

    update features

--- Steve/feature_gen/AvgRank.sql
+++ Steve/feature_gen/AvgRank.sql
@@ -38,3 +38,44 @@ WHERE f."Season" = r."Season" AND
   f."DayNum" > 133 AND
   f."Team" = r."TeamID"
 ;
+
+-- Add feature column to features table
+ALTER TABLE prod.features
+	ADD COLUMN IF NOT EXISTS "OpponentAvgRank" REAL;
+
+-- Regular Season Average Ranking
+WITH rank as (
+    SELECT
+        "Season",
+        "RankingDayNum",
+        "TeamID",
+        AVG("OrdinalRank") as "AvgRank"
+    FROM prod."MasseyOrdinals"
+    GROUP BY "Season", "RankingDayNum", "TeamID"
+)
+UPDATE prod.features as f
+SET "OpponentAvgRank" = r."AvgRank"
+FROM rank as r
+WHERE f."Season" = r."Season" AND
+  f."DayNum" = r."RankingDayNum" AND
+  f."Opponent" = r."TeamID"
+;
+
+-- NCAA Tourney Average Ranking
+WITH rank as (
+    SELECT
+        "Season",
+        "RankingDayNum",
+        "TeamID",
+        AVG("OrdinalRank") as "AvgRank"
+    FROM prod."MasseyOrdinals"
+    WHERE "RankingDayNum" = 133
+    GROUP BY "Season", "RankingDayNum", "TeamID"
+)
+UPDATE prod.features as f
+SET "OpponentAvgRank" = r."AvgRank"
+FROM rank as r
+WHERE f."Season" = r."Season" AND
+  f."DayNum" > 133 AND
+  f."Opponent" = r."TeamID"
+;
