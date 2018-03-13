abdd2f882270a7d5437873a620448d72fd5d23d3
syang <steven.s.yang@gmail.com>
2018-03-12 23:10:34 -0700

    create prod schema

--- pgsql/create_raw.sql
+++ pgsql/create_raw.sql
@@ -1,5 +1,5 @@
 -- Teams
-CREATE TABLE IF NOT EXISTS "Teams" (
+CREATE TABLE IF NOT EXISTS prod."Teams" (
   "TeamID" INT NOT NULL PRIMARY KEY,
   "TeamName" VARCHAR(20),
   "FirstD1Season" INT,
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS "Teams" (
 ;
 
 -- Coaches
-CREATE TABLE IF NOT EXISTS "TeamCoaches" (
+CREATE TABLE IF NOT EXISTS prod."TeamCoaches" (
   "Season" INT,
   "TeamID" INT,
   "FirstDayNum" INT,
@@ -18,14 +18,14 @@ CREATE TABLE IF NOT EXISTS "TeamCoaches" (
 ;
 
 -- Conferences
-CREATE TABLE IF NOT EXISTS "Conferences" (
+CREATE TABLE IF NOT EXISTS prod."Conferences" (
   "ConfAbbrev" VARCHAR(15),
   "Description" VARCHAR(50)
 )
 ;
 
 -- Regular Season Detailed Results
-CREATE TABLE IF NOT EXISTS "RegSeasonDetailedResults" (
+CREATE TABLE IF NOT EXISTS prod."RegSeasonDetailedResults" (
   "Season" INT, "DayNum" INT, "WTeamID" INT, "WScore" INT,
   "LTeamID" INT, "LScore" INT, "WLoc" CHAR(1), "NumOT" INT,
   
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS "RegSeasonDetailedResults" (
 ;
 
 -- Tourney Detailed Results
-CREATE TABLE IF NOT EXISTS "TourneyDetailedResults" (
+CREATE TABLE IF NOT EXISTS prod."TourneyDetailedResults" (
   "Season" INT, "DayNum" INT, "WTeamID" INT, "WScore" INT,
   "LTeamID" INT, "LScore" INT, "WLoc" CHAR(1), "NumOT" INT,
   
@@ -63,7 +63,7 @@ CREATE TABLE IF NOT EXISTS "TourneyDetailedResults" (
 ;
 
 -- Conference Tourney
-CREATE TABLE IF NOT EXISTS "ConferenceTourneyGames" (
+CREATE TABLE IF NOT EXISTS prod."ConferenceTourneyGames" (
   "Season" INT,
   "ConfAbbrev" VARCHAR(16),
   "DayNum" INT,
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS "ConferenceTourneyGames" (
 ;
 
 -- Tourney Seeds
-CREATE TABLE IF NOT EXISTS "TourneySeeds" (
+CREATE TABLE IF NOT EXISTS prod."TourneySeeds" (
   "Season" INT,
   "Seed" VARCHAR(5),
   "TeamID" INT
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS "TourneySeeds" (
 ;
 
 -- Massey Ordinals
-CREATE TABLE IF NOT EXISTS "MasseyOrdinals" (
+CREATE TABLE IF NOT EXISTS prod."MasseyOrdinals" (
   "Season" INT,
   "RankingDayNum" INT,
   "SystemName" VARCHAR(3),
@@ -91,17 +91,17 @@ CREATE TABLE IF NOT EXISTS "MasseyOrdinals" (
 )
 ;
 
--- Features Table
-CREATE TABLE IF NOT EXISTS features_example (
-  "Season" INT,
-  "DayNum" INT,
-  "Team" INT,
-  "Opponent" INT,
-  "Outcome" INT,
-  "Score" INT,
-  "OpponentScore" INT,
-  "NumOT" INT,
-  "WLoc" VARCHAR(1),
-  holdout INT
-)
-;
+-- -- Features Table
+-- CREATE TABLE IF NOT EXISTS features_example (
+--   "Season" INT,
+--   "DayNum" INT,
+--   "Team" INT,
+--   "Opponent" INT,
+--   "Outcome" INT,
+--   "Score" INT,
+--   "OpponentScore" INT,
+--   "NumOT" INT,
+--   "WLoc" VARCHAR(1),
+--   holdout INT
+-- )
+-- ;
