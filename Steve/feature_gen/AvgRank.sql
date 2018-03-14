e9ac8d2043191df1c2911c9dde7876a17281cdd4
syang <steven.s.yang@gmail.com>
2018-03-13 20:34:34 -0700

    update average rank feature
    Includes new logic to account for missing ranking days

--- Steve/feature_gen/AvgRank.sql
+++ Steve/feature_gen/AvgRank.sql
@@ -4,32 +4,46 @@ ALTER TABLE prod.features
 
 -- Regular Season Average Ranking
 WITH rank as (
-    SELECT
-        "Season",
-        "RankingDayNum",
-        "TeamID",
-        AVG("OrdinalRank") as "AvgRank"
-    FROM prod."MasseyOrdinals"
-    GROUP BY "Season", "RankingDayNum", "TeamID"
+  SELECT
+    "Season",
+    "RankingDayNum",
+    "TeamID",
+    AVG("OrdinalRank") as "AvgRank"
+  FROM prod."MasseyOrdinals"
+  GROUP BY "Season", "RankingDayNum", "TeamID"
+  -- Must have more than one available ranking for that day
+  HAVING COUNT("OrdinalRank") > 1
+),
+-- Join to next available DayNum
+daily_rank as (
+  SELECT DISTINCT ON (f."Season", f."DayNum", f."Team")
+    f."Season", f."DayNum", f."Team", r."AvgRank"
+  FROM prod.features as f
+  LEFT JOIN rank as r
+    ON f."Season" = r."Season" and f."Team" = r."TeamID"
+  WHERE f."DayNum" <= r."RankingDayNum"
+  ORDER BY f."Season", f."DayNum", f."Team", r."RankingDayNum"
 )
-UPDATE prod.features as f
+UPDATE prod.features as t
 SET "AvgRank" = r."AvgRank"
-FROM rank as r
-WHERE f."Season" = r."Season" AND
-  f."DayNum" = r."RankingDayNum" AND
-  f."Team" = r."TeamID"
+FROM daily_rank as r
+WHERE t."Season" = r."Season" AND
+  t."DayNum" = r."DayNum" AND
+  t."Team" = r."Team"
 ;
 
 -- NCAA Tourney Average Ranking
 WITH rank as (
-    SELECT
-        "Season",
-        "RankingDayNum",
-        "TeamID",
-        AVG("OrdinalRank") as "AvgRank"
-    FROM prod."MasseyOrdinals"
-    WHERE "RankingDayNum" = 133
-    GROUP BY "Season", "RankingDayNum", "TeamID"
+  SELECT
+    "Season",
+    "RankingDayNum",
+    "TeamID",
+    AVG("OrdinalRank") as "AvgRank"
+  FROM prod."MasseyOrdinals"
+  WHERE "RankingDayNum" = 133
+  GROUP BY "Season", "RankingDayNum", "TeamID"
+	-- Must have more than one available ranking for that day
+  HAVING COUNT("OrdinalRank") > 1
 )
 UPDATE prod.features as f
 SET "AvgRank" = r."AvgRank"
@@ -43,34 +57,48 @@ WHERE f."Season" = r."Season" AND
 ALTER TABLE prod.features
 	ADD COLUMN IF NOT EXISTS "OpponentAvgRank" REAL;
 
--- Regular Season Average Ranking
+-- Regular Season Opponent Average Ranking
 WITH rank as (
-    SELECT
-        "Season",
-        "RankingDayNum",
-        "TeamID",
-        AVG("OrdinalRank") as "AvgRank"
-    FROM prod."MasseyOrdinals"
-    GROUP BY "Season", "RankingDayNum", "TeamID"
+  SELECT
+    "Season",
+    "RankingDayNum",
+    "TeamID",
+    AVG("OrdinalRank") as "AvgRank"
+  FROM prod."MasseyOrdinals"
+  GROUP BY "Season", "RankingDayNum", "TeamID"
+  -- Must have more than one available ranking for that day
+  HAVING COUNT("OrdinalRank") > 1
+),
+-- Join to next available DayNum
+daily_rank as (
+  SELECT DISTINCT ON (f."Season", f."DayNum", f."Opponent")
+    f."Season", f."DayNum", f."Opponent", r."AvgRank"
+  FROM prod.features as f
+  LEFT JOIN rank as r
+    ON f."Season" = r."Season" and f."Opponent" = r."TeamID"
+  WHERE f."DayNum" <= r."RankingDayNum"
+  ORDER BY f."Season", f."DayNum", f."Opponent", r."RankingDayNum"
 )
-UPDATE prod.features as f
+UPDATE prod.features as t
 SET "OpponentAvgRank" = r."AvgRank"
-FROM rank as r
-WHERE f."Season" = r."Season" AND
-  f."DayNum" = r."RankingDayNum" AND
-  f."Opponent" = r."TeamID"
+FROM daily_rank as r
+WHERE t."Season" = r."Season" AND
+  t."DayNum" = r."DayNum" AND
+  t."Team" = r."Opponent"
 ;
 
--- NCAA Tourney Average Ranking
+-- NCAA Tourney Opponent Average Ranking
 WITH rank as (
-    SELECT
-        "Season",
-        "RankingDayNum",
-        "TeamID",
-        AVG("OrdinalRank") as "AvgRank"
-    FROM prod."MasseyOrdinals"
-    WHERE "RankingDayNum" = 133
-    GROUP BY "Season", "RankingDayNum", "TeamID"
+  SELECT
+    "Season",
+    "RankingDayNum",
+    "TeamID",
+    AVG("OrdinalRank") as "AvgRank"
+  FROM prod."MasseyOrdinals"
+  WHERE "RankingDayNum" = 133
+  GROUP BY "Season", "RankingDayNum", "TeamID"
+	-- Must have more than one available ranking for that day
+  HAVING COUNT("OrdinalRank") > 1
 )
 UPDATE prod.features as f
 SET "OpponentAvgRank" = r."AvgRank"
